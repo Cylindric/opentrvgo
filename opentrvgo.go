ff20acd9185c16f3de784f83ddccbf4f7814a703
Cylindric <mark@hanfordonline.co.uk>
2016-11-09 09:26:12 +0000

    Parser workup

--- opentrvgo.go
+++ opentrvgo.go
@@ -12,30 +12,74 @@ type Sample struct {
 	Timestamp         time.Time `json:"timestamp"`
 	Device            string    `json:"device"`
 	Temperature       float64   `json:"temperature"`
-	Humidity          float64   `json:"humidity"`
-	Light             float64   `json:"light"`
-	TargetTemperature float64   `json:"targettemperature"`
-	Valve             float64   `json:"valve"`
-	Occupancy         float64   `json:"occupancy"`
+	Humidity          int       `json:"humidity"`
+	Light             int       `json:"light"`
+	TargetTemperature int       `json:"targettemperature"`
+	Valve             int       `json:"valve"`
+	Occupancy         int       `json:"occupancy"`
 	Battery           float64   `json:"battery"`
+	Vacancy           int       `json:"vacancy"`
 }
 
 // ParseSensorReport attempts to parse a block of data, for example from a serial port, into a Sample record
-func ParseSensorReport(input []byte, smple Sample) (valid bool, err error) {
-	var d Sample
+func ParseSensorReport(input []byte) (sample Sample, err error) {
+	var d map[string]interface{}
 
-	// Stringify the byte array
-	s := strings.TrimSpace(string(input))
+	// Timestamp is now
+	sample.Timestamp = time.Now()
 
-	// If the line isn't actually a JSON response, just return invalid
-	if strings.HasPrefix(s, `{"@"`) == false {
-		return false, fmt.Errorf("Not a valid sensor report")
+	// Stringify the byte array.
+	str := strings.TrimSpace(string(input))
+
+	// If the line isn't actually a JSON response, just return invalid.
+	if strings.HasPrefix(str, `{"@"`) == false {
+		return sample, fmt.Errorf("Not a valid sensor report")
 	}
 
-	// First try to turn the passed-in data from JSON to an object
+	// First try to turn the passed-in data from JSON to an object.
 	if err := json.Unmarshal(input, &d); err != nil {
-		return false, fmt.Errorf("could not parse json.")
+		return sample, fmt.Errorf("could not parse json.")
 	}
 
-	return false, nil
+	// Try to fill the sample with data.
+
+	if _, ok := d["@"]; ok {
+		sample.Device = d["@"].(string)
+	} else {
+		return sample, fmt.Errorf("Record does not contain a device ID field '@'.")
+	}
+
+	if _, ok := d["B|cV"]; ok {
+		sample.Battery = d["B|cV"].(float64) / 100
+	}
+
+	if _, ok := d["H|%"]; ok {
+		sample.Humidity = int(d["H|%"].(float64))
+	}
+
+	if _, ok := d["L"]; ok {
+		sample.Light = int(d["L"].(float64))
+	}
+
+	if _, ok := d["tT|C"]; ok {
+		sample.TargetTemperature = int(d["tT|C"].(float64))
+	}
+
+	if _, ok := d["T|C16"]; ok {
+		sample.Temperature = d["T|C16"].(float64) / 16
+	}
+
+	if _, ok := d["O"]; ok {
+		sample.Occupancy = int(d["O"].(float64))
+	}
+
+	if _, ok := d["vac|h"]; ok {
+		sample.Vacancy = int(d["vac|h"].(float64))
+	}
+
+	if _, ok := d["v|%"]; ok {
+		sample.Valve = int(d["v|%"].(float64))
+	}
+
+	return sample, nil
 }
--- opentrvgo_test.go
+++ opentrvgo_test.go
@@ -3,27 +3,65 @@ package opentrvgo
 import "testing"
 
 func TestParseSensorReport(t *testing.T) {
-	var sample Sample
-	input := []byte(`{"@":"8CD3878ACDCE86BB","+":8,"H|%":48,"O":1,"L":6,"v|%":0}`)
+	input := []byte(`{"@":"8CD3878ACDCE86BB","+":8,"H|%":23,"O":56,"L":34,"v|%":89,"B|cV":1200,"tT|C":67,"T|C16":64,"vac|h":78}`)
 
-	valid, err := ParseSensorReport(input, sample)
+	sample, err := ParseSensorReport(input)
 	if err != nil {
 		t.Error("Expected nil, got ", err)
 	}
-	if valid == false {
-		t.Error("Expected valid==true, got ", valid)
+
+	if sample.Battery != 12 {
+		t.Error("Expected Battery 12, got ", sample.Battery)
+	}
+
+	if sample.Device != "8CD3878ACDCE86BB" {
+		t.Error("Expected Device 8CD3878ACDCE86BB, got ", sample.Device)
+	}
+
+	if sample.Humidity != 23 {
+		t.Error("Expected Humidity 23, got ", sample.Humidity)
+	}
+
+	if sample.Light != 34 {
+		t.Error("Expected Light 34, got ", sample.Light)
+	}
+
+	if sample.Occupancy != 56 {
+		t.Error("Expected Occupancy 56, got ", sample.Occupancy)
+	}
+
+	if sample.TargetTemperature != 67 {
+		t.Error("Expected Target Temperature 67, got ", sample.TargetTemperature)
+	}
+
+	if sample.Temperature != 4 {
+		t.Error("Expected Temperature 4, got ", sample.Temperature)
+	}
+
+	if sample.Vacancy != 78 {
+		t.Error("Expected Vacancy 78, got ", sample.Vacancy)
+	}
+
+	if sample.Valve != 89 {
+		t.Error("Expected Valve 89, got ", sample.Valve)
+	}
+
+}
+
+func TestParseSensorReportFailsWhenMissingSerial(t *testing.T) {
+	input := []byte(`{"+":8,"H|%":48,"O":1,"L":6,"v|%":0}`)
+
+	_, err := ParseSensorReport(input)
+	if err == nil {
+		t.Error("Expected an error")
 	}
 }
 
 func TestParsedSensorReportReturnsErrorOnInvalidJSON(t *testing.T) {
-	var sample Sample
 	input := []byte("invalid")
 
-	valid, err := ParseSensorReport(input, sample)
+	_, err := ParseSensorReport(input)
 	if err == nil {
 		t.Error("Expected an error, got nil")
-	}
-	if valid {
-		t.Error("Expected valid==false, got ", valid)
 	}
 }
